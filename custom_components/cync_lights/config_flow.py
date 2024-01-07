987ad88a37c50aa383e6344952a0cf6cc0e03298
Kyle VanderBeek <kylev@kylev.com>
2024-01-06 17:02:41 -0800

    Better log message.

--- custom_components/cync_lights/config_flow.py
+++ custom_components/cync_lights/config_flow.py
@@ -102,7 +102,7 @@ class CyncConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         except InvalidAuth:
             errors["base"] = "invalid_auth"
         except Exception as e:  # pylint: disable=broad-except
-            _LOGGER.warning("Unexpected exception during auth", exc_info=e)
+            _LOGGER.warning("Unexpected exception two-factor", exc_info=e)
             errors["base"] = "unknown"
         else:
             self.data = info
